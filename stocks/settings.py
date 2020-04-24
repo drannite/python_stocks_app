3e11319f2381080af6b752cbae26f779f72bcd66
Andrew <apankevicius@gmail.com>
2020-04-24 16:34:51 +1000

    addressing api key issue

--- stocks/settings.py
+++ stocks/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = ['stocks-django-app.herokuapp.com']
+ALLOWED_HOSTS = ['stocks-django-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
